252059642022c52be4195ff4a31a4587b778b029
DESKTOP-3GFBU72\DayHR <changyu_liu@dayhr.com>
2019-07-11 18:05:55 +0800

    Update utils.py

--- Python/img2cartton/3_项目中所有代码/fast-style-transfer/tf/src/utils.py
+++ Python/img2cartton/3_项目中所有代码/fast-style-transfer/tf/src/utils.py
@@ -1,8 +1,12 @@
-import scipy.misc, numpy as np, os, sys
+import scipy.misc
+import numpy as np
+import os
+
 
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
     scipy.misc.imsave(out_path, img)
+
 
 def scale_img(style_path, style_scale):
     scale = float(style_scale)
@@ -12,16 +16,19 @@ def scale_img(style_path, style_scale):
     style_target = _get_img(style_path, img_size=new_shape)
     return style_target
 
-def get_img(src, img_size=False):
-   img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
-   if not (len(img.shape) == 3 and img.shape[2] == 3):
-       img = np.dstack((img,img,img))
-   if img_size != False:
-       img = scipy.misc.imresize(img, img_size)
-   return img
+
+def _get_img(src, img_size=False):
+    img = scipy.misc.imread(src, mode='RGB')  # misc.imresize(, (256, 256, 3))
+    if not (len(img.shape) == 3 and img.shape[2] == 3):
+        img = np.dstack((img, img, img))
+    if img_size:
+        img = scipy.misc.imresize(img, img_size)
+    return img
+
 
 def exists(p, msg):
     assert os.path.exists(p), msg
+
 
 def list_files(in_path):
     files = []
@@ -30,4 +37,3 @@ def list_files(in_path):
         break
 
     return files
-
