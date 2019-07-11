e6beb98f305367c6a6085b1d0612c6b771384519
DESKTOP-3GFBU72\DayHR <yang_wu@dayhr.com>
2019-07-11 16:52:28 +0800

    Move the pre-tf2.0 code to the tf directory

--- Python/img2cartton/3_项目中所有代码/fast-style-transfer/tf/src/optimize.py
+++ Python/img2cartton/3_项目中所有代码/fast-style-transfer/tf/src/optimize.py
@@ -1,7 +1,23 @@
-from __future__ import print_function
+# Copyright 2019 ChangyuLiu Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
 import functools
-import vgg, pdb, time
-import tensorflow as tf, numpy as np, os
+import vgg
+import time
+import tensorflow as tf
+import numpy as np
 import transform
 from utils import get_img
 
@@ -10,6 +26,8 @@ CONTENT_LAYER = 'relu4_2'
 DEVICES = 'CUDA_VISIBLE_DEVICES'
 
 # np arr, np arr
+
+
 def optimize(content_targets, style_target, content_weight, style_weight,
              tv_weight, vgg_path, epochs=2, print_iterations=1000,
              batch_size=4, save_path='saver/fns.ckpt', slow=False,
@@ -19,28 +37,32 @@ def optimize(content_targets, style_target, content_weight, style_weight,
     mod = len(content_targets) % batch_size
     if mod > 0:
         print("Train set has been trimmed slightly..")
-        content_targets = content_targets[:-mod] 
+        content_targets = content_targets[:-mod]
 
     style_features = {}
 
-    batch_shape = (batch_size,256,256,3)
+    batch_shape = (batch_size, 256, 256, 3)
     style_shape = (1,) + style_target.shape
     print(style_shape)
 
     # precompute style features
     with tf.Graph().as_default(), tf.device('/cpu:0'), tf.Session() as sess:
-        style_image = tf.placeholder(tf.float32, shape=style_shape, name='style_image')
+        style_image = tf.placeholder(
+            tf.float32, shape=style_shape, name='style_image')
         style_image_pre = vgg.preprocess(style_image)
         net = vgg.net(vgg_path, style_image_pre)
         style_pre = np.array([style_target])
         for layer in STYLE_LAYERS:
-            features = net[layer].eval(feed_dict={style_image:style_pre})
+            features = net[layer].eval(feed_dict={style_image: style_pre})
             features = np.reshape(features, (-1, features.shape[3]))
             gram = np.matmul(features.T, features) / features.size
             style_features[layer] = gram
 
     with tf.Graph().as_default(), tf.Session() as sess:
-        X_content = tf.placeholder(tf.float32, shape=batch_shape, name="X_content")
+        X_content = tf.placeholder(
+            tf.float32,
+            shape=batch_shape,
+            name="X_content")
         X_pre = vgg.preprocess(X_content)
 
         # precompute content features
@@ -54,13 +76,16 @@ def optimize(content_targets, style_target, content_weight, style_weight,
             )
             preds_pre = preds
         else:
-            preds = transform.net(X_content/255.0)
+            preds = transform.net(X_content / 255.0)
             preds_pre = vgg.preprocess(preds)
 
         net = vgg.net(vgg_path, preds_pre)
 
-        content_size = _tensor_size(content_features[CONTENT_LAYER])*batch_size
-        assert _tensor_size(content_features[CONTENT_LAYER]) == _tensor_size(net[CONTENT_LAYER])
+        content_size = _tensor_size(
+            content_features[CONTENT_LAYER]) * batch_size
+        assert _tensor_size(
+            content_features[CONTENT_LAYER]) == _tensor_size(
+            net[CONTENT_LAYER])
         content_loss = content_weight * (2 * tf.nn.l2_loss(
             net[CONTENT_LAYER] - content_features[CONTENT_LAYER]) / content_size
         )
@@ -68,22 +93,32 @@ def optimize(content_targets, style_target, content_weight, style_weight,
         style_losses = []
         for style_layer in STYLE_LAYERS:
             layer = net[style_layer]
-            bs, height, width, filters = map(lambda i:i.value,layer.get_shape())
+            bs, height, width, filters = map(
+                lambda i: i.value, layer.get_shape())
             size = height * width * filters
             feats = tf.reshape(layer, (bs, height * width, filters))
-            feats_T = tf.transpose(feats, perm=[0,2,1])
+            feats_T = tf.transpose(feats, perm=[0, 2, 1])
             grams = tf.matmul(feats_T, feats) / size
             style_gram = style_features[style_layer]
-            style_losses.append(2 * tf.nn.l2_loss(grams - style_gram)/style_gram.size)
+            style_losses.append(
+                2 *
+                tf.nn.l2_loss(
+                    grams -
+                    style_gram) /
+                style_gram.size)
 
-        style_loss = style_weight * functools.reduce(tf.add, style_losses) / batch_size
+        style_loss = style_weight * \
+            functools.reduce(tf.add, style_losses) / batch_size
 
         # total variation denoising
-        tv_y_size = _tensor_size(preds[:,1:,:,:])
-        tv_x_size = _tensor_size(preds[:,:,1:,:])
-        y_tv = tf.nn.l2_loss(preds[:,1:,:,:] - preds[:,:batch_shape[1]-1,:,:])
-        x_tv = tf.nn.l2_loss(preds[:,:,1:,:] - preds[:,:,:batch_shape[2]-1,:])
-        tv_loss = tv_weight*2*(x_tv/tv_x_size + y_tv/tv_y_size)/batch_size
+        tv_y_size = _tensor_size(preds[:, 1:, :, :])
+        tv_x_size = _tensor_size(preds[:, :, 1:, :])
+        y_tv = tf.nn.l2_loss(preds[:, 1:, :, :] -
+                             preds[:, :batch_shape[1] - 1, :, :])
+        x_tv = tf.nn.l2_loss(preds[:, :, 1:, :] -
+                             preds[:, :, :batch_shape[2] - 1, :])
+        tv_loss = tv_weight * 2 * \
+            (x_tv / tv_x_size + y_tv / tv_y_size) / batch_size
 
         loss = content_loss + style_loss + tv_loss
 
@@ -102,13 +137,15 @@ def optimize(content_targets, style_target, content_weight, style_weight,
                 step = curr + batch_size
                 X_batch = np.zeros(batch_shape, dtype=np.float32)
                 for j, img_p in enumerate(content_targets[curr:step]):
-                   X_batch[j] = get_img(img_p, (256,256,3)).astype(np.float32)
+                    X_batch[j] = get_img(
+                        img_p, (256, 256, 3)).astype(
+                        np.float32)
 
                 iterations += 1
                 assert X_batch.shape[0] == batch_size
 
                 feed_dict = {
-                   X_content:X_batch
+                    X_content: X_batch
                 }
 
                 train_step.run(feed_dict=feed_dict)
@@ -124,18 +161,19 @@ def optimize(content_targets, style_target, content_weight, style_weight,
                 if should_print:
                     to_get = [style_loss, content_loss, tv_loss, loss, preds]
                     test_feed_dict = {
-                       X_content:X_batch
+                        X_content: X_batch
                     }
 
-                    tup = sess.run(to_get, feed_dict = test_feed_dict)
-                    _style_loss,_content_loss,_tv_loss,_loss,_preds = tup
+                    tup = sess.run(to_get, feed_dict=test_feed_dict)
+                    _style_loss, _content_loss, _tv_loss, _loss, _preds = tup
                     losses = (_style_loss, _content_loss, _tv_loss, _loss)
                     if slow:
-                       _preds = vgg.unprocess(_preds)
+                        _preds = vgg.unprocess(_preds)
                     else:
-                       saver = tf.train.Saver()
-                       res = saver.save(sess, save_path)
+                        saver = tf.train.Saver()
+                        res = saver.save(sess, save_path)
                     yield(_preds, losses, iterations, epoch)
+
 
 def _tensor_size(tensor):
     from operator import mul
